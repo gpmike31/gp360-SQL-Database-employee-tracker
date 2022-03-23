c85b1e82ea28a64e0fb76cfe26c7e3d395a3baea
gpmike31 <gpmike3105@gmail.com>
2022-03-23 12:29:59 -0500

    added sql seeds

---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+INSERT INTO department
+  (department_name)
+VALUES
+  ('Sales'),
+  ('Engineering'),
+  ('Finance'),
+  ('Legal'),
+
+INSERT INTO role
+  (employeeRole, salary)
+VALUES
+  ('Salesperson', 30000),
+  ('Enginneer', 60000),
+  ('Manager', 70000),
+  ('Lawyer' 90000),
+  ('Accountant', 50000),
+
+INSERT INTO employee
+  (first_name, last_name)
+VALUES
+  ('James', 'Fraser'),
+  ('Jack', 'London'),
+  ('Robert', 'Bruce'),
+  ('Peter', 'Greenaway'),
+  ('Derek', 'Jarman'),
+
